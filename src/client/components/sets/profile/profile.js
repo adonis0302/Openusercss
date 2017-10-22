169c51132ad1b1ca1f137ee95220a382d04e7eda
adonis0302 <topd5508@gmail.com>
2017-10-22 11:50:33 +0200

    Merged profile-card with it's set

--- src/client/components/sets/profile/profile.js
+++ src/client/components/sets/profile/profile.js
@@ -1,33 +1,65 @@
 import {bulmaComponentGenerator as bulma} from 'vue-bulma-components'
 import {mapGetters} from 'vuex'
+import moment from 'moment'
 
-import profileCard from '../../elements/profile-card/profile-card.vue'
+import icon from '../../elements/icon/icon.vue'
 import themeCard from '../../elements/theme-card/theme-card.vue'
 import flushImg from '../../elements/flush-img/flush-img.vue'
 
-import {leftRight} from '../../../src/client/components/animations'
+import {leftRight, topBottom} from '../../../src/client/components/animations'
 
 export default {
   'components': {
-    'b-columns':     bulma('columns', 'div'),
-    'b-column':      bulma('column', 'div'),
-    'b-section':     bulma('section', 'div'),
-    'b-box':         bulma('box', 'div'),
-    'b-button':      bulma('button', 'button'),
-    'b-level':       bulma('level', 'div'),
-    'b-level-left':  bulma('level-left', 'div'),
-    'b-level-right': bulma('level-right', 'div'),
-    profileCard,
+    'b-tile':            bulma('tile', 'div'),
+    'b-container':       bulma('container', 'div'),
+    'b-container-fluid': bulma('container-fluid', 'div'),
+    'b-box':             bulma('box', 'div'),
+    'b-columns':         bulma('columns', 'div'),
+    'b-column':          bulma('column', 'div'),
+    'b-level':           bulma('level', 'div'),
+    'b-level-left':      bulma('level-left', 'div'),
+    'b-level-right':     bulma('level-right', 'div'),
+    'b-section':         bulma('section', 'div'),
+    'b-button':          bulma('button', 'button'),
     themeCard,
-    flushImg
+    flushImg,
+    icon
+  },
+  beforeCreate () {
+    this.$store.dispatch('getFullUser', this.$route.params.id)
   },
   created () {
-    this.$store.dispatch('getThemes')
+    setInterval(() => {
+      this.time = moment()
+    }, 20000)
   },
   'methods': {
     ...leftRight,
+    ...topBottom,
     ...mapGetters([
-      'user'
-    ])
+      'user',
+      'viewedUser'
+    ]),
+    isOnline (date) {
+      return moment(this.time).diff(date) < 600000
+    },
+    viewedUserThemeCount () {
+      if (!this.viewedUser().themes) {
+        return 0
+      }
+
+      return this.viewedUser().themes.length
+    }
+  },
+  data () {
+    return {
+      'time': moment()
+    }
+  },
+  'computed': {
+    lastOnlineDisplay () {
+      // return 'never'
+      return `Last seen ${this.viewedUser().lastSeenReason}, ${moment(this.time).to(this.viewedUser().lastSeen)}`
+    }
   }
 }
