31c29395007a8b74b95cb81f607489f3be8dae6a
adonis0302 <topd5508@gmail.com>
2017-12-03 03:10:26 +0100

    Rework index page temporarily until site filtering

--- src/components/pages/index/index.js
+++ src/components/pages/index/index.js
@@ -1,12 +1,13 @@
 import {bulmaComponentGenerator as bulma} from 'vue-bulma-components'
+import {mapGetters} from 'vuex'
 
 import attributor from '../../components/footer/footer.vue'
 import navbar from '../../components/navbar/navbar.vue'
 import searchField from '../../components/search-field/search-field.vue'
 import chip from '../../components/chip/chip.vue'
-import showcase from '../../components/theme-showcase/theme-showcase.vue'
 import themeCard from '../../components/theme-card/theme-card.vue'
 import flushImg from '../../components/flush-img/flush-img.vue'
+import notification from '../../components/notification/notification.vue'
 
 export default {
   'components': {
@@ -20,57 +21,17 @@ export default {
     attributor,
     themeCard,
     flushImg,
-    showcase,
     navbar,
-    chip
+    chip,
+    notification
+  },
+  beforeMount () {
+    this.$store.dispatch('getLatestThemes', 3)
   },
   'computed': {
-    popularSites () {
-      return [
-        {
-          'domain':       'google.com',
-          'themeCount':   1660,
-          'installCount': 226
-        },
-        {
-          'domain':       'facebook.com',
-          'themeCount':   1522,
-          'installCount': 443
-        },
-        {
-          'domain':       'decentm.com',
-          'themeCount':   1001,
-          'installCount': 262
-        },
-        {
-          'domain':       'apple.com',
-          'themeCount':   771,
-          'installCount': 112
-        }
-      ]
-    },
-
-    popularThemes () {
-      return [
-        {
-          'title':         'A thing',
-          'screenshotUrl': '/img/main.bg-x360.png',
-          'author':        'decentm',
-          'description':   'This is not really a theme, but just a test to see how cards will look when the site gets finished.'
-        },
-        {
-          'title':         'The amazing placeholder',
-          'screenshotUrl': '/img/main.bg-x360.png',
-          'author':        'decentm',
-          'description':   'This is not really a theme, but just a test to see how cards will look when the site gets finished.'
-        },
-        {
-          'title':         'The theme that saves us all',
-          'screenshotUrl': '/img/main.bg-x360.png',
-          'author':        'decentm',
-          'description':   'This is not really a theme, but just a test to see how cards will look when the site gets finished.'
-        }
-      ]
-    }
+    ...mapGetters([
+      'actionErrors',
+      'themes'
+    ])
   }
 }
