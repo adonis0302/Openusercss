29a763db7266dd78625b1a14cfaf9e8084000678
adonis0302 <topd5508@gmail.com>
2017-12-03 08:12:56 +0100

    Client: Wait for router before ssr

--- src/client/js/server.js
+++ src/client/js/server.js
@@ -1,7 +1,12 @@
 import 'babel-polyfill'
 import {app, router} from './vue'
 
-export default async (context) => {
-  router.push(context.url)
-  return app
+export default (context) => {
+  return new Promise((resolve) => {
+    router.push(context.url)
+
+    router.onReady(() => {
+      resolve(app)
+    })
+  })
 }
