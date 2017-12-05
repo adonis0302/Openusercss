5fcea7e6f1a25e524202f7426bd4adf9cf85e0bd
adonis0302 <topd5508@gmail.com>
2017-12-05 07:46:20 +0100

    Revert "Revert "Client: Delete theme UI + modals""
    This reverts commit a94cc9472ad9c5c27ee5aea839efd6190ba10442.

--- src/components/pages/theme/theme.js
+++ src/components/pages/theme/theme.js
@@ -8,43 +8,69 @@ import flushImg from '../../components/flush-img/flush-img.vue'
 import attributor from '../../components/footer/footer.vue'
 import navbar from '../../components/navbar/navbar.vue'
 import notification from '../../components/notification/notification.vue'
+import bInput from '../../components/b-input/b-input.vue'
 
 export default {
   'components': {
-    'b-tile':             bulma('tile', 'div'),
-    'b-container':        bulma('container', 'div'),
-    'b-container-fluid':  bulma('container-fluid', 'div'),
-    'b-box':              bulma('box', 'div'),
-    'b-columns':          bulma('columns', 'div'),
-    'b-column':           bulma('column', 'div'),
-    'b-level':            bulma('level', 'div'),
-    'b-level-left':       bulma('level-left', 'div'),
-    'b-level-right':      bulma('level-right', 'div'),
-    'b-section':          bulma('section', 'div'),
-    'b-button':           bulma('button', 'button'),
-    'b-content':          bulma('content', 'div'),
-    'b-modal':            bulma('modal', 'div'),
-    'b-modal-background': bulma('modal-background', 'div'),
-    'b-modal-content':    bulma('modal-content', 'div'),
-    'b-modal-close':      bulma('modal-close', 'div'),
+    'b-tile':              bulma('tile', 'div'),
+    'b-container':         bulma('container', 'div'),
+    'b-container-fluid':   bulma('container-fluid', 'div'),
+    'b-box':               bulma('box', 'div'),
+    'b-columns':           bulma('columns', 'div'),
+    'b-column':            bulma('column', 'div'),
+    'b-level':             bulma('level', 'div'),
+    'b-level-left':        bulma('level-left', 'div'),
+    'b-level-right':       bulma('level-right', 'div'),
+    'b-section':           bulma('section', 'div'),
+    'b-button':            bulma('button', 'button'),
+    'b-content':           bulma('content', 'div'),
+    'b-modal':             bulma('modal', 'div'),
+    'b-modal-background':  bulma('modal-background', 'div'),
+    'b-modal-content':     bulma('modal-content', 'div'),
+    'b-modal-close':       bulma('modal-close', 'div'),
+    'b-control':           bulma('control', 'div'),
+    'b-card':              bulma('card', 'div'),
+    'b-card-header':       bulma('card-header', 'div'),
+    'b-card-header-title': bulma('card-header-title', 'div'),
+    'b-card-header-icon':  bulma('card-header-icon', 'div'),
+    'b-card-image':        bulma('card-image', 'div'),
+    'b-card-content':      bulma('card-content', 'div'),
+    'b-card-footer':       bulma('card-footer', 'div'),
+    'b-card-footer-item':  bulma('card-footer-item', 'div'),
     attributor,
     navbar,
     flushImg,
     icon,
-    notification
+    notification,
+    bInput
   },
   beforeMount () {
     this.$store.dispatch('getFullTheme', this.$route.params.id)
   },
   data () {
     return {
-      'viewingSource': false
+      'viewingSource': false,
+      'confirmTitle':  '',
+      'showingModal':  false
     }
   },
   'methods': {
     formatMoment,
+    confirmDeleteTheme () {
+      this.confirmTitle = ''
+      this.$modal.show('delete-theme')
+    },
+    cancelDelete () {
+      this.$modal.hide('delete-theme')
+    },
+    deleteTheme () {
+      this.$store.dispatch('deleteTheme', {
+        'id':       this.viewedTheme._id,
+        'redirect': `/profile/${this.viewedTheme.user._id}`
+      })
+    },
     viewSource () {
-      this.viewingSource = !this.viewingSource
+      this.$modal.show('source-viewer')
     },
     installTheme () {
       if (process.env.NODE_ENV === 'development') {
@@ -57,7 +83,8 @@ export default {
   'computed': {
     ...mapGetters([
       'actionErrors',
-      'themes'
+      'themes',
+      'currentUser'
     ]),
     'viewedTheme': {
       'cache': false,
