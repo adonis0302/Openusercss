0370fa507cdc1f31adb535e5979ea94a292a03ac
adonis0302 <topd5508@gmail.com>
2017-12-06 07:22:55 +0100

    Shared: More extensive tests for merge-deep

--- test/merge-deep.js
+++ test/merge-deep.js
@@ -1,5 +1,4 @@
 import test from 'ava'
-import {cloneDeep} from 'lodash'
 import mergeDeep from '../src/shared/merge-deep'
 import state from './shared/state-mock'
 
@@ -64,4 +63,88 @@ test('adds new objects', async (t) => {
   ]
 
   t.deepEqual(merged, expected)
+})
+
+test('does not remove existing objects', async (t) => {
+  const merged = mergeDeep(state.themes, '_id', [
+    {
+      '_id': '5a275431707d23a322cff59f'
+    }
+  ])
+  const expected = state.themes
+
+  t.deepEqual(merged, expected)
+})
+
+test('throws error if no arguments are given', (t) => {
+  t.throws(() => {
+    mergeDeep()
+  })
+})
+
+test('throws error if no source is given', (t) => {
+  t.throws(() => {
+    // eslint-disable-next-line no-undefined
+    mergeDeep(undefined, '_id', [
+      {
+        '_id': '5a275431707d23a322cff588'
+      }
+    ])
+  })
+})
+
+test('throws error if no key is given', (t) => {
+  t.throws(() => {
+    // eslint-disable-next-line no-undefined
+    mergeDeep(state.themes, undefined, [
+      {
+        '_id': '5a275431707d23a322cff588'
+      }
+    ])
+  })
+})
+
+test('throws error if no new data are given', (t) => {
+  t.throws(() => {
+    mergeDeep(state.themes, '_id')
+  })
+})
+
+test('throws error if new data is null', (t) => {
+  t.throws(() => {
+    mergeDeep(state.themes, '_id', null)
+  })
+})
+
+test('throws error if new data is empty', (t) => {
+  t.throws(() => {
+    mergeDeep(state.themes, '_id', [
+      {}
+    ])
+  })
+})
+
+test('throws error if new data is undefined', (t) => {
+  t.throws(() => {
+    // eslint-disable-next-line no-undefined
+    mergeDeep(state.themes, '_id', undefined)
+  })
+})
+
+test('throws error if new data is an object', (t) => {
+  t.throws(() => {
+    mergeDeep(state.themes, '_id', {})
+  })
+})
+
+test('throws error if new data is a number', (t) => {
+  t.throws(() => {
+    mergeDeep(state.themes, '_id', 5)
+  })
+})
+
+test('throws error if new data is string', (t) => {
+  t.throws(() => {
+    mergeDeep(state.themes, '_id', 'Hello!')
+  })
 })
