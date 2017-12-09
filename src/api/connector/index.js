b211c95a8e2e00670c3fcd894f376861830d1533
adonis0302 <topd5508@gmail.com>
2017-12-09 09:03:31 +0100

    fix($api): Create index if it's incorrect

--- src/api/connector/index.js
+++ src/api/connector/index.js
@@ -1,5 +1,6 @@
 import log from 'chalk-console'
 import camo, {connect} from 'camo'
+import {forOwn} from 'lodash'
 import staticConfig from '../../shared/config'
 
 import Theme from './schema/theme'
@@ -12,29 +13,46 @@ const init = async () => {
 
   await connect(connectionUrl)
 
-  camo.getClient().driver().ensureIndex('themes', {
-    'title':       'text',
-    'description': 'text',
-    'scope':       'text',
-    'content':     'text'
-  }, {
-    'weights': {
-      'title':       10,
-      'description': 6,
-      'scope':       4,
-      'content':     2
-    }
-  })
+  const indexes = {
+    'themes': [
+      {
+        'title':       'text',
+        'description': 'text',
+        'scope':       'text',
+        'content':     'text'
+      },
+      {
+        'weights': {
+          'title':       10,
+          'description': 6,
+          'scope':       4,
+          'content':     2
+        }
+      }
+    ],
+    'users': [
+      {
+        'displayname': 'text',
+        'username':    'text'
+      },
+      {
+        'weights': {
+          'displayname': 1,
+          'username':    1
+        }
+      }
+    ]
+  }
 
-  camo.getClient().driver().ensureIndex('users', {
-    'displayname': 'text',
-    'username':    'text'
-  }, {
-    'weights': {
-      'displayname': 1,
-      'username':    1
-    }
-  })
+  try {
+    forOwn(indexes, (value, key) => {
+      camo.getClient().driver().ensureIndex(key, value[0], value[1])
+    })
+  } catch (error) {
+    forOwn(indexes, (value, key) => {
+      camo.getClient().driver().createIndex(key, value[0], value[1])
+    })
+  }
 
   log.info('API database connection established')
 }
