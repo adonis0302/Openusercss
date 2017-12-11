81a0acaa35a47470c89d5034810dda75857be5b5
adonis0302 <topd5508@gmail.com>
2017-12-11 19:30:17 +0100

    fix($api): Give up on theme validation
    With custom elements, theme validation becomes way too complicated. If the user ignored linting
    errors in their IDE, this won't help.

--- src/api/backend/mutations/save-theme.js
+++ src/api/backend/mutations/save-theme.js
@@ -1,9 +1,4 @@
-import stylelint from 'stylelint'
-
 import mustAuthenticate from '../../../shared/enforce-session'
-import {expected} from '../../../shared/custom-errors'
-
-const {LintError} = expected
 
 export default async (root, {token, title, description, content, version, id}, {Session, Theme, User}) => {
   const session = await mustAuthenticate(token, Session)
@@ -11,17 +6,6 @@ export default async (root, {token, title, description, content, version, id}, {
     '_id': session.user._id
   })
   let newTheme = null
-
-  const result = await stylelint.lint({
-    'code':   content,
-    'config': {
-      'extends': 'stylelint-config-recommended-scss'
-    }
-  })
-
-  if (result.results[0].errored) {
-    throw new LintError(result.results[0].warnings)
-  }
 
   if (id) {
     newTheme = await Theme.findOne({
