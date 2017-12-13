21a5836782612f1a204a05696d7ba2143faa3b47
adonis0302 <topd5508@gmail.com>
2017-12-13 23:01:02 +0100

    feat($client): Add spinner element

---
+++ src/components/elements/spinner/spinner.js
@@ -0,0 +1,24 @@
+export default {
+  'props': [
+    'size',
+    'spinning'
+  ],
+  data () {
+    return {
+      'computedStyle': {
+        'width':               this.sizePx(this.size),
+        'height':              this.sizePx(this.size),
+        'border-top-width':    this.sizePx(this.size / 2),
+        'border-bottom-width': this.sizePx(this.size / 2)
+      }
+    }
+  },
+  'methods': {
+    sizePx (value) {
+      if (typeof value === 'string') {
+        return value
+      }
+      return `${value}px`
+    }
+  }
+}
