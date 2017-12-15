47f0ecb8ee329e3a4efa060046b6d43e7a63c2fc
adonis0302 <topd5508@gmail.com>
2017-12-15 01:55:54 +0100

    feat($api): Attempt to send a half-baked email on registration

--- src/api/backend/mutations/register.js
+++ src/api/backend/mutations/register.js
@@ -1,20 +1,57 @@
 import bcrypt from 'bcryptjs'
 import log from 'chalk-console'
 import {escape} from 'html-escaper'
+import {
+  createTransport,
+  sendEmail
+} from '../../email/mailer'
+import env from '../../.env'
+
+const createSendEmail = async (user) => {
+  const account = env.smtp
+  const transportOptions = {
+    'host':       account.host,
+    'port':       account.port,
+    'secure':     account.secure,
+    'requireTls': true,
+    'logger':     true,
+    'debug':      true,
+    'auth':       {
+      'user': account.user,
+      'pass': account.pass
+    }
+  }
+  const transport = await createTransport(transportOptions)
+  const result = await sendEmail(transport, {
+    'to':       user.email,
+    'template': 'registration',
+    'locals':   {
+      'displayname': user.displayname,
+      'link':        'https://decentm.com'
+    }
+  })
+
+  return result
+}
 
 export default async (root, {displayname, email, password}, {User}) => {
   const saltRounds = 14
-  const saltStarted = Date.now()
   const salt = await bcrypt.genSalt(saltRounds)
   const hash = await bcrypt.hash(password, salt)
-
-  log.info(`Salt generation took ${Date.now() - saltStarted}ms`)
   const newUser = User.create({
     'password':    hash,
     'username':    escape(displayname).toLowerCase(),
     'displayname': escape(displayname),
     email
   })
+  const savedUser = await newUser.save()
 
-  return newUser.save()
+  await newUser.delete()
+  try {
+    createSendEmail(savedUser)
+  } catch (error) {
+    log.error(error)
+  }
+
+  return savedUser
 }
