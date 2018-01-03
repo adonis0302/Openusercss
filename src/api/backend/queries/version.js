9ac42ce8db09cf524e0ef95af959101216b9ae41
adonis0302 <topd5508@gmail.com>
2018-01-03 15:44:24 +0100

    refactor($api): Change over to git-revision

--- src/api/backend/queries/version.js
+++ src/api/backend/queries/version.js
@@ -1,25 +1,10 @@
-import gitCb from 'git-rev'
-import pify from 'pify'
-import {zipObject} from 'lodash'
-
-const git = pify(gitCb, {
-  'errorFirst': false
-})
+import git from 'git-revision'
 
 export default async () => {
-  const props = [
-    'revisionLong',
-    'revisionShort',
-    'revisionTag',
-    'revisionBranch'
-  ]
-
-  const results = await Promise.all([
-    git.long(),
-    git.short(),
-    git.tag(),
-    git.branch()
-  ])
-
-  return zipObject(props, results)
+  return {
+    'revisionLong':   git('long'),
+    'revisionShort':  git('short'),
+    'revisionTag':    git('tag'),
+    'revisionBranch': git('branch')
+  }
 }
