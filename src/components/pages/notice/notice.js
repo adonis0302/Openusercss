35773e92b6dc50689a5a535d274f22dc6ca3abb1
adonis0302 <topd5508@gmail.com>
2018-01-06 00:00:57 +0100

    perf(client): Remove unused lifecycle hooks from notice page

--- src/components/pages/notice/notice.js
+++ src/components/pages/notice/notice.js
@@ -1,5 +1,4 @@
 import {bulmaComponentGenerator as bulma} from 'vue-bulma-components'
-import {decode} from 'he'
 
 import oucFooter from '../../components/ouc-footer/ouc-footer.vue'
 import navbar from '../../components/navbar/navbar.vue'
@@ -11,18 +10,5 @@ export default {
     'b-button':    bulma('button', 'div'),
     oucFooter,
     navbar
-  },
-  mounted () {
-    setTimeout(() => {
-      this.showing = true
-      this.email = decode(this.encodedEmail)
-    }, 2500)
-  },
-  data () {
-    return {
-      'showing':      false,
-      'email':        '(please wait...)',
-      'encodedEmail': '&#x67;&#x72;&#x6F;&#x2E;&#x73;&#x73;&#x63;&#x72;&#x65;&#x73;&#x75;&#x6E;&#x65;&#x70;&#x6F;&#x40;&#x6D;&#x74;&#x6E;&#x65;&#x63;&#x65;&#x64;'
-    }
   }
 }
