f71a43bfba152262d43436d67a0845792006fd6a
adonis0302 <topd5508@gmail.com>
2018-01-31 21:11:42 +0100

    ci: Simplify ci config and scripts

--- ci/scripts/bootstrap.sh
+++ ci/scripts/bootstrap.sh
@@ -3,66 +3,26 @@ set -e
 PATH=$PATH":repo/node_modules/.bin:node_modules/.bin"
 
 print_details() {
-  echo
-  echo '=========================================================='
-  echo 'ls: '
-  ls repo
-
-  echo
-  if [ -d "npm-repo-cache/node_modules" ]; then
-    printf 'Cached node_modules size: '
-    du -sh npm-repo-cache/node_modules
-  else
-    echo "Module cache not found, yarn will rebuild."
-    echo "Cache contents:"
-    ls npm-repo-cache
-  fi
-
-  echo '=========================================================='
+  ls -a repo
   uname -nrom
-
-  printf 'Build date: '
   date
-
-  printf 'Working directory: '
   pwd
-
-  printf 'Yarn version: '
   yarn --version
-
-  printf 'NPM version: '
   npm --version
-
-  printf 'Node version: '
   node --version
-
   git --version
   busybox --help | head -1
-
-  printf 'NODE_ENV: '
   echo $NODE_ENV
-
-  printf 'User: '
   whoami
-
-  echo '=========================================================='
   cat /etc/*-release
-
-  echo '=========================================================='
-  echo
 }
 
 install_packages() {
-  echo
-  echo "Installing package(s):" "$@"
   apk --update add $@ --no-progress
 }
 
 prepare() {
   install_packages git
-
-  echo "Linking cached node_modules to repository"
-  ln -s npm-repo-cache/node_modules repo/node_modules
   print_details
 
   cd repo
@@ -71,7 +31,6 @@ prepare() {
     --silent \
     --frozen-lockfile \
     --non-interactive \
-    --modules-folder npm-repo-cache/node_modules \
     --network-timeout 10000 \
     --network-concurrency 3 \
     --production=false \
