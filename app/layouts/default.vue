42ec858777bb7dd0e4dc94a48b84672f1ab2df38
adonis0302 <topd5508@gmail.com>
2018-03-05 10:23:09 +0100

    fix(client): Use full screen height if no content is available

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -17,17 +17,6 @@
 </script>
 
 <style lang="scss" scoped>
-  @import 'node_modules/bulma/sass/utilities/initial-variables';
-  @import '../scss/autocolor';
-  @import '../scss/variables';
-
-  .ouc-app-root {
-    background-color: $white;
-    min-height: calc(100vh - #{map-get($kerning, 'navbar-height')});
-    position: absolute;
-    width: 100%;
-  }
-
   .spacer {
     width: 1rem;
   }
