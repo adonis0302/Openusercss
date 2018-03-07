11fcc04af2bad3adb23e4baa5d0f2d160ec6e940
adonis0302 <topd5508@gmail.com>
2018-03-07 15:41:22 +0100

    refactor(client): Reimplement extension check and help page

--- app/pages/help/index.vue
+++ app/pages/help/index.vue
@@ -9,11 +9,6 @@
       navbar,
       notification,
     },
-    'computed': {
-      extension () {
-        return process.extension
-      },
-    },
   }
 </script>
 
@@ -32,6 +27,10 @@
       background-color: nth($primary, 1);
       color: nth($primary, 2);
     }
+  }
+
+  .is-inline {
+    display: inline;
   }
 
   .has-margin-horizontal {
@@ -55,73 +54,75 @@
           h1 Installing themes
 
         hr
-        .box.is-warning(v-if="!extension")
-          p
-            fa-icon.has-margin-horizontal(icon="exclamation")
+        no-ssr
+          div(v-if="extension")
+            .box.is-primary
+              fa-icon.is-inline(icon="puzzle-piece")
+              p.is-inline(v-if="extension.version")
+                | Detected extension: {{extension.name}}, version {{extension.version}}. You're good to go!
+              p.is-inline(v-if="!extension.version")
+                | Detected extension: {{extension.name}}. You're good to go!
+
+            p Here's some info about what you can do with {{extension.name}}:
+            .content
+              ul
+                li(v-if="extension.capabilities.includes('install-usercss')")
+                  | Since this extension can handle the usercss format, you can
+                  | click "Install with {{extension.name}}" on a theme page to
+                  | install it.
+                li(v-if="extension.capabilities.includes('configure-after-install')")
+                  | Once a theme is installed, you'll be able to configure it in your extension.
+                li(v-if="extension.capabilities.includes('configure-before-install')")
+                  | You'll be able to configure themes before installing them.
+                li(v-if="extension.capabilities.includes('create-usercss') && extension.capabilities.includes('builtin-editor')")
+                  | You can create usercss compliant themes easily with this extension using its builtin code editor.
+                li(v-if="extension.capabilities.includes('edit-usercss') && extension.capabilities.includes('builtin-editor')")
+                  | You can edit already installed themes with this extension using its builtin code editor.
+                li(v-if="extension.capabilities.includes('import-moz-export')")
+                  | You can import themes that user the @moz- export format.
+                li(v-if="extension.capabilities.includes('export-moz-export')")
+                  | You can export themes into the @moz- format.
+                li(v-if="extension.capabilities.includes('update-manual')")
+                  | {{extension.name}} includes an update checker button that lets
+                  | you update installed themes easily.
+                li(v-if="extension.capabilities.includes('update-auto')")
+                  | {{extension.name}} will check for updates for your themes
+                  | and apply them automatically when available.
+                li(v-if="extension.capabilities.includes('export-json-backups')")
+                  | You can export your installed themes into a JSON formatted file
+                  | for safety.
+                li(v-if="extension.capabilities.includes('import-json-backups')")
+                  | You can import your JSON backups into the extension to restore
+                  | what you backed up.
+                li(v-if="extension.capabilities.includes('manage-local')")
+                  | You can view a list of installed themes in the extension and
+                  | disable, enable, edit or uninstall them.
+                li(v-if="extension.capabilities.includes('search-remote')")
+                  | You can search for themes on websites without having to leave
+                  | the extension.
+                li(v-if="extension.capabilities.includes('query-api')")
+                  | You can see a list of your installed themes straight
+                  | from OpenUserCSS without having to open the extension.
+                li(v-if="extension.capabilities.includes('mutate-api')")
+                  | You can disable, enable or uninstall themes straight
+                  | from OpenUserCSS without having to open the extension.
+
+        .box.is-warning(v-show="!extension")
+          fa-icon.is-inline.has-margin-horizontal(icon="exclamation")
+          div.is-inline
             a(href="https://add0n.com/stylus.html", target="_blank", rel="nofollow noopener")
-              | You don't seem to have a compatible extension installed,
-              | or an error occurred.
-              | Click here to install Stylus to be able to use themes!
-        div(v-if="extension")
-          .box.is-primary
-            p(v-if="extension.version")
-              fa-icon(name="puzzle")
-              | Detected extension: {{extension.name}}, version {{extension.version}}. You're good to go!
-            p(v-if="!extension.version")
-              fa-icon(name="puzzle")
-              | Detected extension: {{extension.name}}. You're good to go!
+             | You don't seem to have a compatible extension installed,
+             | or an error occurred.
+             | Click here to install Stylus to be able to use themes!
 
-          p Here's some info about what you can do with {{extension.name}}:
-          .content
-            ul
-              li(v-if="extension.capabilities.includes('install-usercss')")
-                | Since this extension can handle the usercss format, you can
-                | click "Install with {{extension.name}}" on a theme page to
-                | install it.
-              li(v-if="extension.capabilities.includes('configure-after-install')")
-                | Once a theme is installed, you'll be able to configure it in your extension.
-              li(v-if="extension.capabilities.includes('configure-before-install')")
-                | You'll be able to configure themes before installing them.
-              li(v-if="extension.capabilities.includes('create-usercss') && extension.capabilities.includes('builtin-editor')")
-                | You can create usercss compliant themes easily with this extension using its builtin code editor.
-              li(v-if="extension.capabilities.includes('edit-usercss') && extension.capabilities.includes('builtin-editor')")
-                | You can edit already installed themes with this extension using its builtin code editor.
-              li(v-if="extension.capabilities.includes('import-moz-export')")
-                | You can import themes that user the @moz- export format.
-              li(v-if="extension.capabilities.includes('export-moz-export')")
-                | You can export themes into the @moz- format.
-              li(v-if="extension.capabilities.includes('update-manual')")
-                | {{extension.name}} includes an update checker button that lets
-                | you update installed themes easily.
-              li(v-if="extension.capabilities.includes('update-auto')")
-                | {{extension.name}} will check for updates for your themes
-                | and apply them automatically when available.
-              li(v-if="extension.capabilities.includes('export-json-backups')")
-                | You can export your installed themes into a JSON formatted file
-                | for safety.
-              li(v-if="extension.capabilities.includes('import-json-backups')")
-                | You can import your JSON backups into the extension to restore
-                | what you backed up.
-              li(v-if="extension.capabilities.includes('manage-local')")
-                | You can view a list of installed themes in the extension and
-                | disable, enable, edit or uninstall them.
-              li(v-if="extension.capabilities.includes('search-remote')")
-                | You can search for themes on websites without having to leave
-                | the extension.
-              li(v-if="extension.capabilities.includes('query-api')")
-                | You can see a list of your installed themes straight
-                | from OpenUserCSS without having to open the extension.
-              li(v-if="extension.capabilities.includes('mutate-api')")
-                | You can disable, enable or uninstall themes straight
-                | from OpenUserCSS without having to open the extension.
+        hr
+        .content
+          p
+            | For further help, please drop us a line&nbsp;
+            a(href="//forums.openusercss.org", target="_blank") on our community forums
+            | , or visit&nbsp;
+            nuxt-link(to="/contact") the contact page
 
-          hr
-          .content
-            p
-              | For further help, please drop us a line&nbsp;
-              a(href="//forums.openusercss.org", target="_blank") on our community forums
-              | , or visit&nbsp;
-              nuxt-link(to="/contact") the contact page
 
     ouc-footer
 </template>
--- app/plugins/extension-data.js
+++ app/plugins/extension-data.js
@@ -1,5 +1,127 @@
+/* eslint no-console:0 */
 import Vue from 'vue'
 
+import hat from 'hat'
+import {struct,} from 'superstruct'
+import raven from 'raven-js'
+
+let extension = null
+const key = hat()
+const responseValidator = struct({
+  'type':      'string',
+  'key':       'string',
+  'extension': {
+    'name':         'string',
+    'version':      'string?',
+    'capabilities': [
+      'string?',
+    ],
+  },
+})
+const questionValidator = struct({
+  'type':         'string',
+  'key':          'string',
+  'revision':     'object?',
+  'featuresList': {
+    'required': [
+      'string',
+    ],
+    'optional': [
+      'string',
+    ],
+  },
+})
+const featuresList = {
+  'required': [
+    'install-usercss',
+    'configure-after-install',
+  ],
+  'optional': [
+    'event:install-usercss',
+    'event:is-installed',
+    'configure-before-install',
+    'builtin-editor',
+    'create-usercss',
+    'edit-usercss',
+    'import-moz-export',
+    'export-moz-export',
+    'update-manual',
+    'update-auto',
+    'export-json-backups',
+    'import-json-backups',
+    'manage-local',
+    'search-remote',
+    'query-api',
+    'mutate-api',
+  ],
+}
+
+const sendHandshakeQuestion = () => {
+  try {
+    window.postMessage(questionValidator({
+      'type': 'ouc-handshake-question',
+      featuresList,
+      key,
+    }), '*')
+  } catch (error) {
+    console.error('Failed to send handshake question -', error)
+  }
+}
+const attachHandshakeListeners = () => {
+  window.addEventListener('message', (event) => {
+    try {
+      if (event.data && event.data.type === 'ouc-begin-handshake') {
+        sendHandshakeQuestion()
+      }
+
+      if (event.data && event.data.type === 'ouc-handshake-response') {
+        let response = null
+
+        try {
+          response = responseValidator(event.data)
+        } catch (error) {
+          console.error('Failed to validate exetnsion response -', error)
+        }
+        const missingFeatures = []
+
+        featuresList.required.forEach((feature) => {
+          if (!response.extension.capabilities.includes(feature)) {
+            missingFeatures.push(feature)
+          }
+        })
+
+        if (missingFeatures.length) {
+          throw new Error([
+            `${response.extension.name} ${response.extension.version} is not capable of the following features:`,
+            `${missingFeatures.join('\n')}`,
+          ].join('\n'))
+        }
+
+        if (response.key !== key) {
+          throw new Error([
+            'Response key doesn\'t match.\n',
+            `Expected: ${key}`,
+            `Received: ${response.key}`,
+            `Raw data: ${JSON.stringify(response, null, 2)}`,
+          ].join('\n'))
+        }
+
+        extension = response.extension
+      }
+    } catch (error) {
+      console.error(error)
+      raven.captureException(error)
+    }
+  })
+}
+
+if (process.client) {
+  attachHandshakeListeners()
+  sendHandshakeQuestion()
+}
+
 Vue.mixin({
-  'extension': () => process.extension,
+  'computed': {
+    'extension': () => extension,
+  },
 })
