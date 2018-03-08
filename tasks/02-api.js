7aa9b26bbe60fe066dd85e7cbb01d642e079df93
adonis0302 <topd5508@gmail.com>
2018-03-08 09:37:34 +0100

    refactor(build): Simplify API build process

--- tasks/02-api.js
+++ tasks/02-api.js
@@ -1,45 +1,26 @@
 import gulp from 'gulp'
-import pump from 'pump'
 
 import webpack from 'webpack'
 import webpackStream from 'webpack-stream'
 import webpackConfig from '../api.webpack.config.babel'
 
-const sources = {
-  'api': [
-    'api/index.js',
-  ],
-}
-
 gulp.task('api:prod', () => {
-  return pump([
-    gulp.src(sources.api),
-    webpackStream(webpackConfig({
-      'env':   'production',
-      'watch': false,
-    }), webpack),
-    gulp.dest('./build'),
-  ])
+  return webpackStream(webpackConfig({
+    'env':   'production',
+    'watch': false,
+  }), webpack)
 })
 
 gulp.task('api:fast', () => {
-  return pump([
-    gulp.src(sources.api),
-    webpackStream(webpackConfig({
-      'env':   'development',
-      'watch': false,
-    }), webpack),
-    gulp.dest('./build'),
-  ])
+  return webpackStream(webpackConfig({
+    'env':   'development',
+    'watch': false,
+  }), webpack)
 })
 
 gulp.task('api:watch', (done) => {
-  return pump([
-    gulp.src(sources.api),
-    webpackStream(webpackConfig({
-      'env':   'development',
-      'watch': true,
-    }), webpack),
-    gulp.dest('./build'),
-  ])
+  return webpackStream(webpackConfig({
+    'env':   'development',
+    'watch': true,
+  }), webpack)
 })
