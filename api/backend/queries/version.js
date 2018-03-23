06d59a641f68e8a37a20c15813473c03c988b2fa
adonis0302 <topd5508@gmail.com>
2018-03-23 01:01:20 +0100

    refactor(api): Bake git revision data into the runtime code

--- api/backend/queries/version.js
+++ api/backend/queries/version.js
@@ -1,10 +1,3 @@
-import git from 'git-revision'
-
 export default async () => {
-  return {
-    'revisionLong':   git('long'),
-    'revisionShort':  git('short'),
-    'revisionTag':    git('tag'),
-    'revisionBranch': git('branch'),
-  }
+  return OUC.version
 }
--- backpack.config.js
+++ backpack.config.js
@@ -1,8 +1,20 @@
+const {DefinePlugin,} = require('webpack')
+const git = require('git-revision')
+
 module.exports = {
   webpack (config, options, webpack,) {
     config.entry.main = [
       './api/index.js',
     ]
+
+    config.plugins.push(new DefinePlugin({
+      'OUC.version': {
+        'revisionLong':   JSON.stringify(git('long')),
+        'revisionShort':  JSON.stringify(git('short')),
+        'revisionTag':    JSON.stringify(git('tag')),
+        'revisionBranch': JSON.stringify(git('branch')),
+      },
+    }))
 
     return config
   },
