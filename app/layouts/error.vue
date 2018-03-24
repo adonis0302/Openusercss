84d0af52754b5230c593449bb5ea0a2b122433b1
adonis0302 <topd5508@gmail.com>
2018-03-24 06:03:21 +0100

    refactor(client): Encode debug messages on the error page

--- app/layouts/error.vue
+++ app/layouts/error.vue
@@ -8,47 +8,65 @@
     'props': [
       'error',
     ],
+    'methods': {
+      encode (string) {
+        return Buffer.from(JSON.stringify(string)).toString('base64')
+      },
+      select (event) {
+        if (process.client) {
+          const range = document.createRange()
+
+          range.selectNode(event.target)
+          window.getSelection().removeAllRanges()
+          window.getSelection().addRange(range)
+        }
+      },
+    },
   }
 </script>
+
+<style lang="scss" scoped>
+  pre {
+    word-wrap: break-word;
+    white-space: pre-line;
+  }
+</style>
 
 <template lang="pug">
   div.ouc-route-root
     .container
       .section
         .columns
-          .column
+          .column.is-6
             h1 {{error.statusCode}}
             h4 {{error.message}}
             hr
             .ouc-error-container(:class="['status-' + error.statusCode]")
               .ouc-error-description(v-if="error.statusCode === 404")
-                p Route "{{$route.fullPath}}" is not attached to any particular page.
+                p
+                  | Route "{{$route.fullPath | truncate(50)}}" is not attached
+                  | to any particular page.
                 p Either you clicked on a mistyped link or this page has been moved.
 
               .ouc-error-description(v-if="error.statusCode !== 404")
                 p An unknown error occurred while rendering "{{error.path}}".
 
             br
-            p If that's not enough, you can check these themes out ^.^
+            nuxt-link(to="/")
+              | If that's not enough, click here to return to the home page ^.^
             p Or if you're going back to the Internet, take these with you:&nbsp;&nbsp;
               fa-icon(icon="chess-pawn")
               fa-icon(icon="quidditch")
 
-            hr
-            p
-              | Please include the following information when submitting an issue:
+            div(v-if="error.statusCode !== 404")
+              hr
+              p
+                | Please include the information on the right when submitting an issue!
             br
-            pre {{error}}
-          .column
-            //- .column(v-for="(theme, index) in limitBy(themes, 3)")
-              theme-card(:data-index="index", direction="horizontal", card-class="is-primary", :theme-id="theme._id").has-bottom-margin
-                .tile.is-parent(slot="content")
-                  .tile.is-child
-                    .columns
-                      .column
-                        h4 {{theme.title}}
-                        br
-                        h6 By {{theme.user.displayname}}
-                        h6 Last updated {{theme.lastUpdate | moment('from', 'now')}}
+          .column.is-6
+            h4 Debugging information
+            hr
+            pre(@click="select") {{encode(error)}}
+
     ouc-footer
 </template>
