33a86b2577e97e212260aee7e13b84a8e46696cb
adonis0302 <topd5508@gmail.com>
2018-03-31 15:24:55 +0200

    fix(client): Fix blinking while loading images, add raw img tag

--- app/components/bits/progressive-image.vue
+++ app/components/bits/progressive-image.vue
@@ -1,45 +1,87 @@
 <script>
   export default {
     'props': {
-      'height': [
-        Number,
-        String,
-      ],
-      'width': [
-        Number,
-        String,
-      ],
+      'height': {
+        'type': [
+          Number,
+          String,
+        ],
+        'default': '100%',
+      },
+      'width': {
+        'type': [
+          Number,
+          String,
+        ],
+        'default': '100%',
+      },
       'src':         String,
       'placeholder': String,
       'size':        {
         'type':    String,
         'default': 'contain',
       },
+      'raw': {
+        'type':    Boolean,
+        'default': false,
+      },
+    },
+    'computed': {
+      commonStyle () {
+        return {
+          'height': `${this.height}`,
+          'width':  `${this.width}`,
+        }
+      },
     },
     data () {
       return {
-        'commonStyle': {
-          'height': `${this.height}`,
-          'width':  `${this.width}`,
-        },
         'style': {
-          'height':            `${this.height}`,
-          'width':             `${this.width}`,
+          'height':            '100%',
+          'width':             '100%',
           'background-image':  `url("${this.placeholder}")`,
           'background-repeat': 'no-repeat',
           'background-size':   this.size,
-          'filter':            'blur(2px)',
+          'transition':        'filter .3s ease-in-out',
+          'filter':            'blur(4px)',
+        },
+        'rawStyle': {
+          'height':     '100%',
+          'width':      '100%',
+          'transition': 'filter .3s ease-in-out',
+          'filter':     'blur(4px)',
         },
       }
     },
     mounted () {
-      this.style['background-image'] = `url("${this.src}")`
-      Reflect.deleteProperty(this.style, 'filter')
+      const $image = document.createElement('img')
+
+      $image.src = this.src
+      $image.classList.add('is-invisible')
+
+      $image.onload = () => {
+        if (this.raw) {
+          this.$refs.raw.src = this.src
+          this.rawStyle.filter = ''
+        } else {
+          this.style['background-image'] = `url("${this.src}")`
+          this.style.filter = ''
+        }
+
+        $image.remove()
+      }
+
+      if (this.raw) {
+        this.$refs.raw.append($image)
+      } else {
+        this.$refs.main.appendChild($image)
+      }
     },
   }
 </script>
 
 <template lang="pug">
-  .ouc-responsive-image-wrapper(:style="commonStyle")
-    .ouc-responsive-image(:style="style")
+  .ouc-responsive-image-wrapper(v-if="!raw", :style="commonStyle")
+    .ouc-responsive-image(:style="style", ref="main")
+  img.ouc-responsive-image-raw(v-else, ref="raw", :style="rawStyle", :src="placeholder")
 </template>
