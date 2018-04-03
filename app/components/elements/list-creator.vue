71c958ce59edfa8e0e481501721974df875e36c8
adonis0302 <topd5508@gmail.com>
2018-04-04 01:26:47 +0200

    refactor(client): Redesign list-creator, fix null value

--- app/components/elements/list-creator.vue
+++ app/components/elements/list-creator.vue
@@ -28,6 +28,10 @@
     },
     'computed': {
       listFull () {
+        if (!this.list) {
+          this.list = []
+        }
+
         return this.list.length >= this.maxItems
       },
     },
@@ -61,32 +65,43 @@
 
 <template lang="pug">
   div.ouc-list-creator-wrapper
-    .tile.ouc-list-creator-item.has-bottom-margin.is-parent.is-vertical.is-paddingless(v-for="(item, index) in list")
-      .tile
-        .tile.is-child.is-11
-          .control.has-icons-left(v-if="typeof item === 'string'")
+    div(v-for="(item, index) in list")
+      .columns.is-mobile(v-if="typeof item === 'string'")
+        .column.is-11
+          .control.has-icons-left
             fa-icon.icon(:icon="icon")
-            b-input(
+            input.input(
               v-model="list[index]",
               @input="changeItem",
               :placeholder="placeholder"
             )
-          .tile(v-if="typeof item === 'object' && supportObjects")
-            .control.has-icons-left(v-for="(value, key) in item", v-if="key !== '__typename'")
-              fa-icon.icon(:icon="icon")
-              b-input(
-                v-model="list[index][key]",
-                @input="changeItem"
-              )
-        .tile.is-child.is-1
-          button.button.is-pulled-right.is-danger(@click="removeItem(index)", type="button") X
-    .tile.ouc-list-creator-object-item.is-parent.is-paddingless(v-if="supportObjects")
-      .tile.is-child
-        button.button.is-primary(v-if="!listFull", @click="addItem", type="button") Add string {{itemName}}
-      .tile.is-child
-        button.button.is-primary(v-if="!listFull", @click="addObject", type="button") Add object {{itemName}}
 
-    .tile.ouc-list-creator-array-item.is-parent.is-paddingless(v-if="!supportObjects")
+        .column.is-1
+          button.button.is-pulled-right.is-danger(@click="removeItem(index)", type="button")
+            fa-icon(icon="times")
+
+      .columns.is-mobile(v-if="typeof item === 'object' && supportObjects")
+        .column.is-11
+          .columns
+            .column(v-for="(value, key) in item", v-if="key !== '__typename'")
+              .control.has-icons-left
+                fa-icon.icon(:icon="icon")
+                input.input(
+                  v-model="list[index][key]",
+                  @input="changeItem"
+                )
+
+        .column.is-1
+          button.button.is-pulled-right.is-danger(@click="removeItem(index)", type="button")
+            fa-icon(icon="times")
+
+    br
+    .ouc-list-creator-object-item(v-if="supportObjects")
+      button.button.is-primary(v-if="!listFull", @click="addItem", type="button") Add string {{itemName}}
+      | &nbsp;
+      button.button.is-primary(v-if="!listFull", @click="addObject", type="button") Add object {{itemName}}
+
+    .tile.ouc-list-creator-array-item.is-parent.is-paddingless(v-else)
       .tile.is-child
         button.button.is-primary(v-if="!listFull", @click="addItem", type="button") Add {{itemName}}
 </template>
