798c86f94192bbea01e7b53fb031ddea0c8f214b
adonis0302 <topd5508@gmail.com>
2018-04-04 01:21:56 +0200

    refactor(client): Add separate image-carousel component

---
+++ app/components/elements/image-carousel.vue
@@ -0,0 +1,107 @@
+<script>
+  import progressiveImage from '~/components/bits/progressive-image.vue'
+
+  export default {
+    'components': {
+      progressiveImage,
+    },
+
+    'props': {
+      'value': Array,
+    },
+
+    data () {
+      return {
+        'flickity': null,
+      }
+    },
+
+    'watch': {
+      items () {
+        this.$nextTick(this.rerender)
+      },
+    },
+
+    'computed': {
+      items () {
+        return this.value.map((item) => this.proxyImage(item))
+      },
+      options () {
+        return {
+          'wrapAround':      true,
+          'contain':         true,
+          'fullscreen':      true,
+          'cellAlign':       'center',
+          'pageDots':        false,
+          'prevNextButtons': false,
+        }
+      },
+    },
+
+    mounted () {
+      this.Flickity = require('flickity')
+      this.init()
+    },
+
+    beforeDestroy () {
+      this.flickity.destroy()
+      this.flickity = null
+    },
+
+    'methods': {
+      init () {
+        this.flickity = new this.Flickity(this.$refs.flickity, this.options)
+        this.$emit('init', this.flickity)
+      },
+
+      resize () {
+        this.flickity.resize()
+      },
+
+      reposition () {
+        this.flickity.reposition()
+      },
+
+      rerender () {
+        this.destroy()
+        this.init()
+      },
+
+      destroy () {
+        this.flickity.destroy()
+        this.flickity = null
+      },
+
+      reloadCells () {
+        this.flickity.reloadCells()
+      },
+
+      data () {
+        return this.Flickity.data(this.$refs.flickity)
+      },
+    },
+  }
+</script>
+
+<style lang="scss" scoped>
+  .carousel {
+    &:not(.flickity-enabled) {
+      height: 15rem;
+      clip-path: polygon(0 0, 100% 0, 100% 15rem, 0 15rem);
+    }
+  }
+</style>
+
+<template lang="pug">
+  .carousel(ref="flickity")
+    progressive-image.carousel-cell(
+      v-for="(image, index) in items",
+      :key="image + index",
+      :src="image.large",
+      :placeholder="image.small",
+      width="100%",
+      height="15rem",
+      size="cover",
+      position="top left"
+    )
+</template>
