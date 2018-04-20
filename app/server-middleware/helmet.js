903f4df597b398bce86f6c54a3b14df113706255
adonis0302 <topd5508@gmail.com>
2018-04-20 02:48:32 +0200

    refactor(client): Only send helmet headers in production

--- app/server-middleware/helmet.js
+++ app/server-middleware/helmet.js
@@ -1,78 +1,68 @@
 const helmet = require('helmet')
 
-const cspOptions = {
-  'directives': {
-    'defaultSrc': [
-      "'self'",
-      'openusercss.org',
-      'openusercss.com',
-    ],
-    'scriptSrc': [
-      "'self'",
-      'openusercss.org',
-      'openusercss.com',
-      'pwk.decentm.com',
-      'sentry.io',
-    ],
-    'styleSrc': [
-      "'self'",
-      "'unsafe-inline'",
-      'openusercss.org',
-      'openusercss.com',
-    ],
-    'imgSrc': [
-      "'self'",
-      'data:',
-      'imageproxy.openusercss.org',
-      'imageproxy.openusercss.com',
-      'gravatar.com',
-    ],
-    'connectSrc': [
-      'openusercss.org',
-      'openusercss.com',
-      'api.openusercss.org',
-      'api.openusercss.com',
-      'imageproxy.openusercss.org',
-      'imageproxy.openusercss.com',
-      'pwk.decentm.com',
-      'gravatar.com',
-      'sentry.io',
-    ],
-    'fontSrc': [
-      'data:',
-      "'self'",
-    ],
-    'workerSrc': [
-      "'self'",
-      'blob:',
-    ],
-  },
-}
+if (process.env.NODE_ENV === 'production') {
+  const cspOptions = {
+    'directives': {
+      'defaultSrc': [
+        "'self'",
+        'openusercss.org',
+        'openusercss.com',
+      ],
+      'scriptSrc': [
+        "'self'",
+        'openusercss.org',
+        'openusercss.com',
+        'pwk.decentm.com',
+        'sentry.io',
+      ],
+      'styleSrc': [
+        "'self'",
+        "'unsafe-inline'",
+        'openusercss.org',
+        'openusercss.com',
+      ],
+      'imgSrc': [
+        "'self'",
+        'data:',
+        'imageproxy.openusercss.org',
+        'imageproxy.openusercss.com',
+        'gravatar.com',
+      ],
+      'connectSrc': [
+        'openusercss.org',
+        'openusercss.com',
+        'api.openusercss.org',
+        'api.openusercss.com',
+        'imageproxy.openusercss.org',
+        'imageproxy.openusercss.com',
+        'pwk.decentm.com',
+        'gravatar.com',
+        'sentry.io',
+      ],
+      'fontSrc': [
+        'data:',
+        "'self'",
+      ],
+      'workerSrc': [
+        "'self'",
+        'blob:',
+      ],
+    },
+  }
 
-if (process.env.NODE_ENV === 'development') {
-  cspOptions.directives.defaultSrc.push('localhost')
-  cspOptions.directives.imgSrc.push('localhost')
-  cspOptions.directives.connectSrc.push('localhost:*')
-  cspOptions.directives.connectSrc.push('ws://localhost:*')
-  cspOptions.directives.scriptSrc = [
-    ...cspOptions.directives.scriptSrc,
-    "'unsafe-inline'",
-    "'unsafe-eval'",
-  ]
+  module.exports = helmet({
+    'contentSecurityPolicy': cspOptions,
+    'dnsPrefetchControl':    {
+      'allow': true,
+    },
+    'frameguard': {
+      'action': 'deny',
+    },
+    'hsts': {
+      'maxAge': 60 * 60 * 24 * 60,
+    },
+    'referrerPolicy': {
+      'policy': 'strict-origin-when-cross-origin',
+    },
+  })
 }
-
-module.exports = helmet({
-  'contentSecurityPolicy': cspOptions,
-  'dnsPrefetchControl':    {
-    'allow': true,
-  },
-  'frameguard': {
-    'action': 'deny',
-  },
-  'hsts': {
-    'maxAge': 60 * 60 * 24 * 60,
-  },
-  'referrerPolicy': {
-    'policy': 'strict-origin-when-cross-origin',
-  },
-})
