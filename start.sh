51ee633ae9e0edae58306c6dafc05f753bb0c951
adonis0302 <topd5508@gmail.com>
2018-05-02 09:17:50 +0200

    refactor(build): First class startscript

--- start.sh
+++ start.sh
@@ -1,20 +1,86 @@
 #!/bin/bash
 set -e
 
+################################################################################
+## Variables
+################################################################################
+
 PATH=$PATH":node_modules/.bin:.ignored/bin"
 CLEANUP_NEEDED=false
+LOG_FILE=.ignored/logs/preflight.log
+LOG_DIR=$(echo $LOG_FILE | rev | cut -d '/' -f2- | rev)
+SHORT=$(git rev-parse --short HEAD)
+TAG=$(git describe --always --tag --abbrev=0)
+AGENT="OpenUserCSS startscript v$TAG (github.com/OpenUserCSS/openusercss.org)"
+TRACE=false
+ERROR_HANDLING=false
+
+RED='\033[0;31m'
+ORANGE='\033[0;33m'
+CYAN='\033[0;36m'
+RESET='\033[0m'
 
 ################################################################################
-## Functions
+## Argument parsing and usage hints
 ################################################################################
 
-info_prev_length=0
+usage_example () {
+  printf '%15s\t%-s\n' "$1" "$2"
+}
+
+usage () {
+  printf "OpenUserCSS development startup script version %s\n" "$TAG"
+  printf "Usage: ./start.sh\n"
+  printf "\n"
+  usage_example "-h --help" "Shows this help message"
+  usage_example "-t --trace" "Enables verbose logging to file"
+  printf ""
+}
+
+while [ "$1" != "" ]; do
+  PARAM=`echo $1 | awk -F= '{print $1}'`
+  VALUE=`echo $1 | awk -F= '{print $2}'`
+  case $PARAM in
+    -h | --help)
+      usage
+      exit
+      ;;
+    -t | --trace)
+      TRACE=true
+      ;;
+    -f | --file)
+      LOG_FILE=$VALUE
+      ;;
+    *)
+      printf "Unknown parameter \"$PARAM\"\n\n"
+      usage
+      exit 1
+      ;;
+  esac
+  shift
+done
+
+################################################################################
+## Logging
+################################################################################
+
+log () {
+  tee /dev/fd/3
+}
+
+reset_file () {
+  rm -f $1
+  touch $1
+}
+
+newline () {
+  printf "\\n" | tee /dev/fd/3
+}
 
 info () {
   content=$1
   expect_sameline=$2
   carriage="\\n"
-
 
   if [ $info_prev_length -gt 0 ]; then
     for (( i=1; i <= $info_prev_length + 7; i++)); do
@@ -30,115 +96,140 @@ info () {
     info_prev_length=${#content}
   fi
 
-  printf "[INFO] %s$carriage" "$content"
+  printf "$CYAN[INFO]$RESET %s$carriage" "$content" | log
 }
+
+################################################################################
+## Error handling
+################################################################################
+
+error () {
+  local msg=$1
+  local code=$2
+
+  if [ -z "$msg" ]; then
+    printf "Error message _must_ be provided\n"
+    exit 1
+  fi
+
+  if [ ! -z "$code" ]; then
+    newline
+    remove_trap
+    set -e
+
+    printf "===========================\\n" | tee /dev/fd/3
+    printf "| Mayday, mayday, mayday! |\\n" | tee /dev/fd/3
+    printf "===========================\\n" | tee /dev/fd/3
+
+    system_info
+
+    if $CLEANUP_NEEDED; then
+      CLEANUP_NEEDED=false
+      cleanup || printf "Cleanup failed\n"
+    fi
+
+    printf "\\n$RED[ERROR]$RESET %s\\n" "$msg" | tee /dev/fd/3
+
+    print_trace
+    newline
+
+    if [ "$TRACE" = false ]; then
+      error "Detailed tracing was not enabled for this run. To get the most help,"
+      error "please re-run this script with $0 -t and submit that one."
+      newline
+    fi
+
+    printf "You can help fix this error if you want by sharing your logs with me.\n" 1>&3
+    printf "Upload script logs (size: %s) to https://termbin.com? (y/N) " "$(du -ah $LOG_FILE | cut -f1)" 1>&3
+
+    read -n 1 upload
+
+    newline
+    if [[ $upload =~ ^[Yy]$ ]]; then
+      upload_logfile
+    fi
+
+    exit "$code"
+  else
+    printf "$ORANGE[WARN]$RESET %s\\n" "$msg" | tee /dev/fd/3
+  fi
+}
+
+create_trap () {
+  if [ "$ERROR_HANDLING" = false ]; then
+    info "Error handling enabled"
+    trap 'error "An error occurred on line $LINENO" 1' INT ERR TERM
+    ERROR_HANDLING=true
+  fi
+}
+
+remove_trap () {
+  if [ "$ERROR_HANDLING" = true ]; then
+    info "Error handling disabled"
+    trap - INT ERR TERM
+    ERROR_HANDLING=false
+  fi
+}
+
+print_trace () {
+  local TRACE_TEXT=""
+  local CP=$$
+
+  while true
+  do
+    local CMDLINE
+    local PP
+    local TRACE_TEXT="$TRACE_TEXT [$CP]:$CMDLINE\n"
+
+    CMDLINE=$(cat /proc/$CP/cmdline | tr -d '\0')
+    PP=$(grep PPid /proc/$CP/status | awk '{ print $2; }')
+    if [ "$CP" = "1" ]; then
+      break
+    fi
+    CP=$PP
+  done
+
+  echo | tee /dev/fd/3
+  echo "Backtrace of '$0'" | tee /dev/fd/3
+  printf "$TRACE_TEXT" | tac | grep -n ":" | tee /dev/fd/3
+}
+
+upload_logfile () {
+  info "Uploading, please wait..." true
+  upload_result=$(cat $LOG_FILE | nc termbin.com 9999 | tr -d '\0')
+
+  info "Uploaded logs available at $upload_result"
+}
+
+system_info () {
+  info "System info:"
+  info "Operating system: $(uname -o)"
+  info "Kernel: $(uname -s)"
+  info "Release: $(uname -r)"
+  info "Version: $(uname -v)"
+  info "Processor: $(uname -p)"
+  info "Platform: $(uname -i)"
+  newline
+  info "/etc/*-release: $(cat /etc/*-release)"
+}
+
+################################################################################
+## Script lifecycle
+################################################################################
 
 cleanup () {
   info "Removing development domains from hosts file"
   sudo hostess del dev.openusercss.local
   sudo hostess del api.dev.openusercss.local
 
+  jobs -p 1>&3
+  info "Waiting for $(jobs -p | wc -l) jobs to complete..."
+  countdown 10 "Timed out"
   wait
 }
 
-error () {
-  msg=$1
-  code=$2
-
-  if [ ! -z "$code" ]; then
-    printf "\\n[ERROR] Mayday, mayday, mayday!\\n"
-
-    if $CLEANUP_NEEDED; then
-      CLEANUP_NEEDED=false
-      cleanup || error "Cleanup failed"
-    fi
-
-    printf "\\n[ERROR] %s\\n\\n" "$msg"
-    exit "$code"
-  else
-    printf "[ERROR] %s\\n" "$msg"
-  fi
-}
-
-check_binary () {
-  bin=$1
-  test=$2
-  url=$3
-
-  info "Checking $bin..." true
-
-  if [ -z "$test" ]; then
-    error "Test parameters for $bin are not specified. This is a developer error, please open an issue!" 1
-  fi
-
-  if [ ! -z "$bin" ]; then
-    if ! type "$bin" > /dev/null 2> /dev/null; then
-      if [ ! -z "$url" ]; then
-        info "Binary $bin is not available in \$PATH."
-        info "Download URL available, receiving from $url"
-
-        wget \
-          -r \
-          -q \
-          --tries=3 \
-          -nc \
-          --progress=bar \
-          --show-progress \
-          -U "OpenUserCSS startscript v$VERSION (github.com/OpenUserCSS/openusercss.org)" \
-          -O .ignored/bin/"$bin" \
-          "$url"
-
-        chmod +x .ignored/bin/"$bin"
-      else
-        error "$bin is not available in \$PATH" 1
-      fi
-    fi
-  fi
-
-  command=$(printf "%s" "$test" | cut -d '#' -f1)
-  expected=$(printf "%s" "$test" | cut -d '#' -f2)
-
-  output=$($command)
-  outputstatus=$?
-
-  if [[ ! "$output" = *"$expected"* ]]; then
-    printf "\\n"
-    error "Expected $expected"
-    error "Got      $output"
-    error "Test output of binary \"$bin\" is not valid." 1
-  fi
-
-  if [ $outputstatus -gt 0 ]; then
-    error "Error while testing $bin"
-    error "Command $command returned error code $outputstatus" $outputstatus
-  fi
-
-  info "$bin is suitable"
-}
-
-check_env () {
-  if [ "$(id -u)" = 0 ]; then
-    error "This script must _not_ be run as root." 1
-  fi
-
-  while read -r line; do
-    bin=$(printf "%s" "$line" | cut -d ';' -f1)
-    test=$(printf "%s" "$line" | cut -s -d ';' -f2)
-    url=$(printf "%s" "$line" | cut -s -d ';' -f3)
-
-    check_binary "$bin" "$test" "$url"
-  done < script_requirements.txt
-
-  set +e
-  docker info > /dev/null
-  dockerstatus=$?
-  set -e
-
-  if [ $dockerstatus -gt 0 ]; then
-    error "The Docker daemon is either not running, or $(whoami) has no access to it" $dockerstatus
-  fi
-
-  info "Preflight inspection complete."
+print_self () {
+  cat $0
 }
 
 keep_root () {
@@ -154,22 +245,30 @@ keep_root () {
 }
 
 initialise () {
+  mkdir -p "$LOG_DIR"
+  reset_file $LOG_FILE
+  exec 3>&1 1>>${LOG_FILE} 2>&1
+
+  if [ "$TRACE" = true ]; then
+    PS4='${BASH_LINENO[0]} - ${FUNCNAME[0]}() +'
+    set -o xtrace
+  fi
+
+  info "OpenUserCSS startscript $TAG ($SHORT) on $(date "+%Y.%m.%d - %H:%M.%S")"
+  create_trap
+
   info "Starting before-takeoff checklist"
   check_env
 
   info "Adding development domains to hosts file"
 
-  set +e
-  trap - INT
-  sudo printf "[INFO] Sudo session initialised\n"
-  sudostatus=$?
-  trap 'error "CTRL+C pressed" 1' INT
-  set -e
+  sudo printf "[INFO] Sudo session initialised\n" | tee /dev/fd/3
+  local sudostatus=$?
 
   if [ $sudostatus -gt 0 ]; then
-    error "Root access not granted. You must manually add the following entries:"
-    error "127.0.0.1 dev.openusercss.local"
-    error "127.0.0.1 api.dev.openusercss.local"
+    info "Root access not granted. You must manually add the following entries:"
+    info "127.0.0.1 dev.openusercss.local"
+    info "127.0.0.1 api.dev.openusercss.local"
     info "Add them, then press ENTER to continue"
     read
   else
@@ -179,11 +278,12 @@ initialise () {
     CLEANUP_NEEDED=true
   fi
 
+  local hostsfile
   hostsfile=$(cat /etc/hosts)
 
   if [[ ! "$hostsfile" = *"dev.openusercss.local"* ]] \
      || [[ ! "$hostsfile" = *"api.dev.openusercss.local"* ]]; then
-    printf "\\n"
+    newline
     error "Can't find development hosts in /etc/hosts"
     error "Hosts file test failed" 1
   fi
@@ -191,9 +291,154 @@ initialise () {
   info "Before-takeoff checklist complete"
 }
 
+################################################################################
+## Environment checking
+################################################################################
+
+check_binary () {
+  local bin=$1
+  local test=$2
+  local url=$3
+  local postprocess=$4
+  local installed=true
+
+  if [ -z "$test" ]; then
+    error "Test parameters for $bin are not specified. This is a developer error, please open an issue!" 1
+  fi
+
+  if [ ! -z "$bin" ]; then
+    if ! type "$bin" > /dev/null 2> /dev/null; then
+      if [ ! -z "$url" ]; then
+        info "Binary $bin is not available in \$PATH."
+        info "Download URL available, receiving from $url"
+
+        if [ -z "$postprocess" ]; then
+          wget \
+            -r \
+            -q \
+            --tries=3 \
+            -nc \
+            --progress=bar \
+            --show-progress \
+            -U $AGENT \
+            -O .ignored/bin/"$bin" \
+            "$url"
+        else
+          wget \
+            -r \
+            -q \
+            --tries=3 \
+            -nc \
+            --progress=bar \
+            --show-progress \
+            -U $AGENT \
+            -O .ignored/temp/"$bin" \
+            "$url"
+        fi
+
+        chmod +x .ignored/bin/"$bin"
+      else
+        # error "$bin is not available in \$PATH" 1
+        installed=false
+      fi
+    fi
+  else
+    error "A binary has not been properly specified. This is a developer error, please open an issue!"
+    error "Line: $*" 1
+  fi
+
+  local command
+  local expected
+  local output
+  local outputstatus
+
+  command=$(printf "%s" "$test" | cut -d '#' -f1)
+  expected=$(printf "%s" "$test" | cut -d '#' -f2)
+
+  set +e
+  output=$(sh -c "$command")
+  outputstatus=$?
+  set -e
+
+  local prefix="[✔]"
+  local suffix="suitable"
+  local colour=$CYAN
+  local success=true
+
+  if [[ ! "$output" = *"$expected"* ]] || [ "$installed" = false ]; then
+    prefix="[✘]"
+    suffix="foul"
+    colour=$RED
+    success=false
+  fi
+
+  if [ $outputstatus -gt 0 ]; then
+    error "Error while testing $bin"
+    error "Command $command returned error code $outputstatus" $outputstatus
+    colour=$RED
+    success=false
+    return
+  fi
+
+  printf "${colour}%s${RESET} %15s\t%-s\n" $prefix $bin $suffix | log
+
+  if [ "$success" = false ]; then
+    all_bin_result=false
+    printf "============================================================\n" | log
+    printf "%-30s\t%-30s\\n" "Exptected text to include:" "Got:" | log
+    diff <(echo $expected) <(echo $output) -dy -W 60 | log
+    printf "============================================================\n" | log
+  fi
+
+  if [ "$installed" = false ]; then
+    error "$bin is not available in \$PATH" 1
+  fi
+}
+
+check_env () {
+  if [ "$(id -u)" = 0 ]; then
+    error "This script must _not_ be run as root." 1
+  fi
+
+  all_bin_result=true
+
+  while read -r line; do
+    local bin
+    local test
+    local url
+    local postprocess
+
+    bin=$(printf "%s" "$line" | cut -d ';' -f1)
+    test=$(printf "%s" "$line" | cut -s -d ';' -f2)
+    url=$(printf "%s" "$line" | cut -s -d ';' -f3)
+    postprocess=$(printf "%s" "$line" | cut -s -d ';' -f4)
+
+    check_binary "$bin" "$test" "$url" "$postprocess"
+  done < script_requirements.txt
+
+  if [ "$all_bin_result" = false ]; then
+    error "Failed to validate one or more binaries" 1
+  fi
+
+  set +e
+  docker info > /dev/null
+  dockerstatus=$?
+  set -e
+
+  if [ $dockerstatus -gt 0 ]; then
+    error "The Docker daemon is either not running, or $(whoami) has no access to it" $dockerstatus
+  fi
+
+  info "Preflight inspection complete."
+}
+
+################################################################################
+## Utility
+################################################################################
+
 countdown () {
-  target=$1
-  message=$2
+  local target=$1
+  local message=$2
 
   for (( target; $target >= 1; target-- )); do
 		info "[$target]" true
@@ -207,24 +452,18 @@ countdown () {
 ## Main
 ################################################################################
 
-trap 'error "CTRL+C pressed" 1' INT
 initialise
-
 countdown 3 "Takeoff"
 
-SHORT=$(git rev-parse --short HEAD)
-TAG=$(git describe --always --tag --abbrev=0)
-
 printf "\\n"
-info "OpenUserCSS startscript $TAG ($SHORT)"
 keep_root
 
 info "Rotate"
-docker-compose -f dev.stack.yml build || error "Building the stack image failed" 1
+docker-compose -f dev.stack.yml build | tee /dev/fd/3 || error "Building the stack image failed" 1
 
 countdown 2 "Gear up"
 
-docker-compose -f dev.stack.yml run --rm dev.openusercss || error \
+docker-compose -f dev.stack.yml run --rm dev.openusercss | tee /dev/fd/3 || error \
   "A runtime error occurred - There is likely additional logging output above" 1
 
 cleanup
