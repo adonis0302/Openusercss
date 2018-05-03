8594306d25ef63437f56716a9bededcf1a5ede7e
adonis0302 <topd5508@gmail.com>
2018-05-03 09:24:38 +0200

    feat(client): Add shaking entry animation to error page

--- app/layouts/error.vue
+++ app/layouts/error.vue
@@ -4,6 +4,7 @@
   import assert from 'assert'
 
   export default {
+    'transition': 'fade',
     'components': {
       oucFooter,
     },
@@ -197,58 +198,59 @@
 </style>
 
 <template lang="pug">
-  div.ouc-route-root
-    .container
-      .section
-        .columns
-          .column.is-6
-            h1 {{error.statusCode}}
-            h4 {{error.message}}
-            hr
-            .ouc-error-container(:class="['status-' + error.statusCode]")
-              .ouc-error-description(v-if="error.statusCode === 404")
-                p
-                  | Route "{{$route.fullPath | truncate(50)}}" is not attached
-                  | to any particular page.
-                p Either you clicked on a mistyped link or this page has been moved.
-
-              .ouc-error-description(v-if="error.statusCode !== 404")
-                p An unknown error occurred while rendering "{{error.path || $route.fullPath}}".
-
-            br
-            nuxt-link.has-text-primary(to="/")
-              | If that's not enough, click here to return to the home page ^.^
-            p Or if you're going back to the Internet, take these with you:&nbsp;&nbsp;
-              fa-icon(icon="shield-alt")
-              fa-icon(icon="quidditch")
-
-            div(v-if="error.statusCode !== 404")
+  transition(name="error")
+    .ouc-route-root
+      .container
+        .section
+          .columns
+            .column.is-6
+              h1 {{error.statusCode}}
+              h4 {{error.message}}
               hr
-              p
-                | Please include the information on the right when submitting an issue!
-            br
-          .column.is-6
-            h4 Debugging information
-            br
-            button.button.is-primary(v-if="!showCommentBox", @click="showCommentBox = true")
-              | Leave a comment
-            div(v-if="showCommentBox")
-              textarea.textarea.input(
-                type="text",
-                v-model="userMessage",
-                :placeholder="commentPlaceholder"
-              )
-              p
-                br
-                | When you're done typing, please copy this wall of text
-                | below and&nbsp;
-                a.has-text-primary(
-                  href="//github.com/OpenUserCSS/openusercss.org/issues",
-                  target="_blank",
-                  rel="noopener"
-                ) submit an issue on GitHub!
-            hr
-            pre(@click="select") {{encode(extendedError)}}
+              .ouc-error-container(:class="['status-' + error.statusCode]")
+                .ouc-error-description(v-if="error.statusCode === 404")
+                  p
+                    | Route "{{$route.fullPath | truncate(50)}}" is not attached
+                    | to any particular page.
+                  p Either you clicked on a mistyped link or this page has been moved.
 
-    ouc-footer
+                .ouc-error-description(v-if="error.statusCode !== 404")
+                  p An unknown error occurred while rendering "{{error.path || $route.fullPath}}".
+
+              br
+              nuxt-link.has-text-primary(to="/")
+                | If that's not enough, click here to return to the home page ^.^
+              p Or if you're going back to the Internet, take these with you:&nbsp;&nbsp;
+                fa-icon(icon="shield-alt")
+                fa-icon(icon="quidditch")
+
+              div(v-if="error.statusCode !== 404")
+                hr
+                p
+                  | Please include the information on the right when submitting an issue!
+              br
+            .column.is-6
+              h4 Debugging information
+              br
+              button.button.is-primary(v-if="!showCommentBox", @click="showCommentBox = true")
+                | Leave a comment
+              div(v-if="showCommentBox")
+                textarea.textarea.input(
+                  type="text",
+                  v-model="userMessage",
+                  :placeholder="commentPlaceholder"
+                )
+                p
+                  br
+                  | When you're done typing, please copy this wall of text
+                  | below and&nbsp;
+                  a.has-text-primary(
+                    href="//github.com/OpenUserCSS/openusercss.org/issues",
+                    target="_blank",
+                    rel="noopener"
+                  ) submit an issue on GitHub!
+              hr
+              pre(@click="select") {{encode(extendedError)}}
+
+      ouc-footer
 </template>
