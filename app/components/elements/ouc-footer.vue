1d148d3ea105075c4d8a4f02f06555252608448d
adonis0302 <topd5508@gmail.com>
2018-05-07 09:11:26 +0200

    refactor: Add back Paypal link in footer

--- app/components/elements/ouc-footer.vue
+++ app/components/elements/ouc-footer.vue
@@ -44,10 +44,6 @@
     flex-direction: column;
   }
 
-  .ouc-im-broke-wrapper {
-    @include brand-gradient(map-get($tones, 'brand-dark'), map-get($tones, 'patreon'), 40%, 42.5%);
-  }
-
   .bg-transparent {
     background: transparent;
   }
@@ -72,9 +68,39 @@
   .v--modal .box {
     margin-top: 4rem;
   }
+
+  .ouc-im-broke-wrapper {
+    @include brand-gradient(map-get($tones, 'paypal'), map-get($tones, 'patreon'), 40%, 47.5%);
+  }
 </style>
 
 <template lang="pug">
+  mixin donation-button(link, text, imageSrc, imagePlaceholder, width)
+    - text = text || 'Donate!'
+    - width = width || '1.75rem'
+
+    .tile.is-child
+      a.button.is-fullheight.is-hidden-touch(href=link, target="_blank")&attributes(attributes)
+        b #{text}
+        .has-padding
+        progressive-image(
+          src=imageSrc,
+          placeholder=imagePlaceholder,
+          height="1.75rem",
+          width=width,
+          position="center center"
+        )
+      a.button.is-fullheight.is-fullwidth.is-hidden-desktop.has-padding-1(href=link, target="_blank")&attributes(attributes)
+        b #{text}
+        .has-padding
+        progressive-image(
+          src=imageSrc,
+          placeholder=imagePlaceholder,
+          height="1.5rem",
+          width=width,
+          position="center center"
+        )
+
   div.ouc-footer-wrapper
     modal(
       name="changelog-viewer",
@@ -85,60 +111,51 @@
         div(v-html="changelog")
 
     .is-vertical.ouc-im-broke-wrapper
-      .container.ouc-im-broke
-        .tile.is-parent.is-paddingless
-          .tile.is-child.is-pulled-left
-            .box.ouc-im-broke-begging.is-borderless.is-shadowless.bg-transparent.has-text-white
-              | OpenUserCSS needs your support!
-          .tile.is-child
-            a.button.is-patreon.is-pulled-right.is-fullheight.is-hidden-touch(href="//patreon.com/DecentM", target="_blank")
-              b Become a patron on
-              .has-padding
-              progressive-image(
-                src="/img/patreon.icon-x64.png",
-                placeholder="/img/patreon.icon-x16.png",
-                height="1.75rem",
-                width="1.75rem"
-              )
-            a.button.is-patreon.is-pulled-right.is-fullheight.is-fullwidth.is-hidden-desktop.has-padding-1(href="//patreon.com/DecentM", target="_blank")
-              b Become a patron on
-              .has-padding
-              progressive-image(
-                src="/img/patreon.icon-x64.png",
-                placeholder="/img/patreon.icon-x16.png",
-                height="1.5rem",
-                width="1.5rem"
-              )
-
-      .footer.is-vertical.ouc-footer-content
+      .ouc-im-broke.is-fullheight
         .container
-          .columns
-            .column.has-text-left
-              .tile
-                p
-                  | Copyright&nbsp;&copy;&nbsp;{{new Date().getFullYear()}}&nbsp;
-                  a(href="//github.com/DecentM", target="_blank") DecentM&#32;
-                  | and&#32;
-                  a(href="//github.com/OpenUserCSS/openusercss.org/blob/master/README.md#contributors", target="_blank") Contributors
-              .tile
-                p
-                  a(href="//forums.openusercss.org/topic/5/privacy-policy") Privacy policy
-                  | &#32;|&#32;
-                  a(href="//forums.openusercss.org/topic/6/terms-of-service") Terms of service
-                  | &#32;|&#32;
-                  router-link(to="/notice") Notice
-            .column.has-text-centered
-              a(href="//github.com/OpenUserCSS", target="_blank")
-                fa-icon(icon="code-branch")
-                | GitHub
-              div(v-if="dev")
-                nuxt-link.has-text-primary(to="/test")
-                  | Open test page
-            .column.has-text-right
-              router-link(to="/contact")
-                fa-icon(icon="envelope")
-                | Contact the administrator
-              p API version: {{version.revisionTag}}
-              p Client version: {{$pkg.version}}&nbsp;
-                a(@click.prevent="$modal.show('changelog-viewer')") (changelog)
+          .tile.is-parent.is-paddingless
+            +donation-button(
+              "//www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=NQE35LHY6NKS6",
+              "Donate through",
+              "/img/paypal.icon-x64.png",
+              "/img/paypal.icon-x16.png"
+            ).is-paypal.is-pulled-left
+            +donation-button(
+              "//patreon.com/DecentM",
+              "Become a patron on",
+              "/img/patreon.icon-x64.png",
+              "/img/patreon.icon-x16.png"
+            ).is-patreon.is-pulled-right
+
+    .footer.is-vertical.ouc-footer-content
+      .container
+        .columns
+          .column.has-text-left
+            .tile
+              p
+                | Copyright&nbsp;&copy;&nbsp;{{new Date().getFullYear()}}&nbsp;
+                a(href="//github.com/DecentM", target="_blank") DecentM&#32;
+                | and&#32;
+                a(href="//github.com/OpenUserCSS/openusercss.org/blob/master/README.md#contributors", target="_blank") Contributors
+            .tile
+              p
+                a(href="//forums.openusercss.org/topic/5/privacy-policy") Privacy policy
+                | &#32;|&#32;
+                a(href="//forums.openusercss.org/topic/6/terms-of-service") Terms of service
+                | &#32;|&#32;
+                router-link(to="/notice") Notice
+          .column.has-text-centered
+            a(href="//github.com/OpenUserCSS", target="_blank")
+              fa-icon(icon="code-branch")
+              | GitHub
+            div(v-if="dev")
+              nuxt-link.has-text-primary(to="/test")
+                | Open test page
+          .column.has-text-right
+            router-link(to="/contact")
+              fa-icon(icon="envelope")
+              | Contact the administrator
+            p API version: {{version.revisionTag}}
+            p Client version: {{$pkg.version}}&nbsp;
+              a(@click.prevent="$modal.show('changelog-viewer')") (changelog)
 </template>
